09ce9f4c246082de799fb9bbf8805239c5614fec
HoaPhan <hoa.phan.df@renesas.com>
2020-12-24 15:56:43 +0700

    update train_model.py to save numpy array after load image

--- train_model.py
+++ train_model.py
@@ -14,14 +14,35 @@ print(tf.__version__)
 
 num_class=48
 
-train_labels = get_label("train",num_class)
-validation_labels = get_label("validation",num_class)
 
-train_images = get_images("train",num_class)
-validation_images = get_images("validation",num_class)
+print("loading train label")
+if os.path.isfile("train_labels.npy"):
+    train_labels = np.load('train_labels.npy') 
+else:
+    train_labels = get_label("train",num_class)
+    np.save('train_labels.npy', train_labels)
 
-print(train_labels)
-print(validation_labels)
+print("loading validation label")
+if os.path.isfile("validation_labels.npy"):
+    validation_labels = np.load('validation_labels.npy')
+else:
+    validation_labels = get_label("validation",num_class)
+    np.save('validation_labels.npy', validation_labels)
+
+print("loading train image")
+if os.path.isfile("train_images.npy"):
+    train_images = np.load('train_images.npy')
+else:
+    train_images = get_images("train",num_class)
+    np.save('train_images.npy', train_images)
+
+print("loading validation image")
+if os.path.isfile("validation_images.npy"):
+    validation_images = np.load('validation_images.npy')
+else:
+    validation_images = get_images("validation",num_class)
+    np.save('validation_images.npy', validation_images)
+
 
 
 
@@ -34,6 +55,7 @@ checkpoint_dir = os.path.dirname(checkpoint_path)
 # Create a callback that saves the model's weights
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,save_weights_only=True,verbose=1)
 # Train model by train image 
+print("training model")
 model.fit(train_images, train_labels, epochs=50,validation_data=(validation_images, validation_labels), callbacks=[cp_callback])
 
 
